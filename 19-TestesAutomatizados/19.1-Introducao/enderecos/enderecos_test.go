bbf08866bc98bc1ecfa9ab44d42c0468ccf95833
Leonardo Mayca <leomt8440@gmail.com>
2025-04-08 22:05:10 -0300

    43. Teste Unitário Básico

---
+++ 19-TestesAutomatizados/19.1-Introducao/enderecos/enderecos_test.go
@@ -0,0 +1,18 @@
+// TESTE DE UNIDADE
+package enderecos
+
+import "testing"
+
+// Test + Nome da Função
+func TestTipoDeEndereco(t *testing.T) {
+	enderecoParaTeste := "Avenida Paulista"
+	tipoDeEnderecoEsperado := "Avenida"
+	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
+
+	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
+		t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s",
+			tipoDeEnderecoEsperado,
+			tipoDeEnderecoRecebido,
+		)
+	}
+}
