b235abdfaaeb7d1dc2e93724f08375598fba8195
Leonardo Mayca <leomt8440@gmail.com>
2025-04-12 11:27:21 -0300

    52. Renderizando página HTML

--- 22-HTML/main.go
+++ 22-HTML/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+	u := usuario{"João", "joao.pedro@gmail.com"}
+	templates.ExecuteTemplate(w, "home.html", u)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob("*.html"))
+
 	http.HandleFunc("/home", home)
 
+	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
